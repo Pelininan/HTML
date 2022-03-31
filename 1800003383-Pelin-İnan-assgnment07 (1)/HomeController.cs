685abc9888cdab30ba0c73b20106d3fb29a6177c
Pelininan <71101949+Pelininan@users.noreply.github.com>
2022-03-31 13:56:24 +0300

    HTML
    building a site with html (no css)

---
+++ 1800003383-Pelin-İnan-assgnment07 (1)/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication10.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Menu()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult form1(int txtId, string txtName, string chkAddress)
+        {
+            ViewBag.Id = txtId;
+            ViewBag.Name = txtName;
+            if (chkAddress != null)
+                ViewBag.Addon = "Selected";
+            else
+                ViewBag.Addon = "Not Selected";
+
+            return View("Index");
+        }
+    }
+}
---
+++ 1800003383-Pelin-İnan-assgnment07 (1)/traceRestaurant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication10.Models
+{
+    public class traceRestaurant
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+    }
+}
